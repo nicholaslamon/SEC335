dd44ffbc1c534a2b363e75db2970205a8ea39441
nicholaslamon <70776282+nicholaslamon@users.noreply.github.com>
2023-01-23 17:50:43 -0500

    Update ipsweep.bash

--- ipsweep.bash
+++ ipsweep.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Augment the sequence and ip address ranges for whatever network this is pointing at.
+
 for ip in $(seq 2 50);
 do
 ping -c 1 -t 64 "10.0.5.$ip" > /dev/null 2>&1 && 
