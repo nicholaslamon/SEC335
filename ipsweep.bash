5b72b73113511f80ffe62700b7a85eab6ead3f3e
nicholaslamon <70776282+nicholaslamon@users.noreply.github.com>
2023-01-23 17:49:52 -0500

    Create ipsweep.bash

---
+++ ipsweep.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+for ip in $(seq 2 50);
+do
+ping -c 1 -t 64 "10.0.5.$ip" > /dev/null 2>&1 && 
+echo "Ping Status of 10.0.5.$ip : Success" || 
+echo "Ping Status of 10.0.5.$ip : Failed" 
+done > output.txt
